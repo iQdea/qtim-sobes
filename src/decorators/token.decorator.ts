0bf88f5c11e31291202767e13271731a01fb0d15
iQdea <i.tolyan1506@yandex.ru>
2024-05-02 14:26:27 +0300

    fix decorator AccessToken

--- src/decorators/token.decorator.ts
+++ src/decorators/token.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ExtractJwt } from 'passport-jwt';
 
 export const AccessToken = createParamDecorator((data: string, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  return request.cookies['access_token'];
+  return request.cookies['access_token'] || ExtractJwt.fromAuthHeaderAsBearerToken()(request);
 });
